fed62358139c3a04365e26b0cd22bf4e6c84b131
Casimir de Bruijn <casimir.db@gmail.com>
2019-10-07 14:14:58 +0200

    Updates

---
+++ component---src-pages-404-tsx-a8a7f89461ca46485ad3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{239:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(234),o=n(236);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-a8a7f89461ca46485ad3.js.map
