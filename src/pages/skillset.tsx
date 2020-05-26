f3689990f6441578fa94418788edb5e508f48b5a
Casimir de Bruijn <casimir.db@gmail.com>
2020-05-26 13:47:50 +0200

    Reordered the content in "Skillset" page

--- src/pages/skillset.tsx
+++ src/pages/skillset.tsx
@@ -74,37 +74,6 @@ const SkillsetPage: () => JSX.Element = (): JSX.Element => (
         />
         <CenteredBlock>
           <ContentSeparator>
-            <h1>Education</h1>
-            <ExperienceCard
-              name="Digital Media & Communication"
-              type="HBO"
-              icon="icons/hu_logo.jpg"
-              dateStart="2015-09-05"
-              dateEnd="2019-02-04"
-              description="This is where my interest in programming started, as well as my first steps into graphic design. I learned about marketing and doing user research. Halfway through my final year I decided to drop out due to lack of motivation as I wanted to pursue learning more about programming."
-              backgroundColor="#298CCD"
-            />
-          </ContentSeparator>
-          <ContentSeparator>
-            <h1>Professional experience</h1>
-            <Grid>
-              {experienceData.edges.map(
-                ({ node: { frontmatter } }: IExperienceEdge, i: number) => (
-                  <ExperienceCard
-                    name={frontmatter.title}
-                    type={frontmatter.type}
-                    description={frontmatter.excerpt}
-                    icon={`icons/${frontmatter.title.toLocaleLowerCase()}_logo.png`}
-                    backgroundColor={frontmatter.backgroundColor}
-                    dateStart={frontmatter.dateStart}
-                    dateEnd={frontmatter.dateEnd}
-                    key={i}
-                  />
-                )
-              )}
-            </Grid>
-          </ContentSeparator>
-          <ContentSeparator>
             <h1>So, you want to know more?</h1>
             <p>
               From my experience so far, programming is about 20% actually
@@ -119,16 +88,17 @@ const SkillsetPage: () => JSX.Element = (): JSX.Element => (
         <ContentSeparator>
           <Grid
             columns="repeat(auto-fill, minmax(20rem, 1fr))"
-            maxWidth="100rem"
+            maxWidth="160rem"
           >
             {skillsetData.edges.map(
-              ({ node: { frontmatter } }: ISkillsetEdge, i: number) => (
+              ({ node: { frontmatter } }: ISkillsetEdge, i: number): JSX.Element => (
                 <SkillCard
                   level={frontmatter.level || 0}
                   title={frontmatter.title}
                   description={frontmatter.excerpt}
                   time={frontmatter.time}
                   key={i}
+                  index={i}
                 />
               )
             )}
@@ -152,6 +122,41 @@ const SkillsetPage: () => JSX.Element = (): JSX.Element => (
             <StyledLink to="/about">about me section</StyledLink>, I have some
             experience with other programs outside of programming.
           </p>
+        </CenteredBlock>
+        <CenteredBlock>
+          <ContentSeparator>
+            <h1>Education</h1>
+            <ExperienceCard
+              name="Digital Media & Communication"
+              type="HBO"
+              icon="icons/hu_logo.jpg"
+              dateStart="2015-09-05"
+              dateEnd="2019-02-04"
+              description="This is where my interest in programming started, as well as my first steps into graphic design. I learned about marketing and doing user research. Halfway through my final year I decided to drop out due to lack of motivation as I wanted to pursue learning more about programming."
+              backgroundColor="#298CCD"
+              index={0}
+            />
+          </ContentSeparator>
+          <ContentSeparator>
+            <h1>Professional experience</h1>
+            <Grid>
+              {experienceData.edges.map(
+                ({ node: { frontmatter } }: IExperienceEdge, i: number): JSX.Element => (
+                  <ExperienceCard
+                    name={frontmatter.title}
+                    type={frontmatter.type}
+                    description={frontmatter.excerpt}
+                    icon={`icons/${frontmatter.title.toLocaleLowerCase()}_logo.png`}
+                    backgroundColor={frontmatter.backgroundColor}
+                    dateStart={frontmatter.dateStart}
+                    dateEnd={frontmatter.dateEnd}
+                    key={i}
+                    index={i}
+                  />
+                )
+              )}
+            </Grid>
+          </ContentSeparator>
         </CenteredBlock>
       </Layout>
     )}
