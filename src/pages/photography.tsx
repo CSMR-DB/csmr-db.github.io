cf3f68449e4b406107eaae17e0fcc1bd832191f8
Casimir de Bruijn <casimir.db@gmail.com>
2020-05-27 13:08:00 +0200

    Removed some copy and repositioned images

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -4,19 +4,20 @@ import { Layout } from '../components/Layout'
 import { SEO } from '../components/SEO'
 import { Grid } from '../components/Grid'
 import { DynamicImage } from '../components/DynamicImage'
+import { CenteredBlock } from '../components/CenteredBlock'
 
 const SecondPage: () => JSX.Element = (): JSX.Element => (
   <Layout>
     <SEO title="Photography" />
-    <article>
-      <h1>Photography</h1>
-      <p>
-        Something I also enjoy doing, but don't do that often. As part of my
-        personal story I figured it could be fun to add some shots that I think
-        worked out quite acceptably.
-      </p>
-    </article>
-    <Grid columns={3}>
+    <CenteredBlock>
+      <article>
+        <h1>Photography</h1>
+        <p>
+          Something I also enjoy doing, but don't do that often.
+        </p>
+      </article>
+    </CenteredBlock>
+    <Grid columns={'repeat(auto-fill, minmax(32rem,1fr))'}>
       <DynamicImage path="photography/" />
     </Grid>
   </Layout>
