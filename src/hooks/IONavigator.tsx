aef61a8f152970d158e68a71464a929d98caada9
Casimir de Bruijn <casimir.db@gmail.com>
2020-05-27 13:06:17 +0200

    Removed calls to console.log()

--- src/hooks/IONavigator.tsx
+++ src/hooks/IONavigator.tsx
@@ -5,10 +5,8 @@ import { useKeyPressNavigation } from './useKeyPressNavigation.hook';
 
 export function IONavigator({ children, routes }: { children: React.ReactNode, routes: IRouteObject[] }): JSX.Element {
   const currentPage: string = typeof window !== 'undefined' ? window.location.pathname : '';
-  console.log(currentPage)
 
   const navigationPage: string = useKeyPressNavigation(routes)
-  console.log(navigationPage)
   if ( navigationPage !== '' && navigationPage !== currentPage ) {
     // tslint:disable-next-line: no-floating-promises
     navigate(navigationPage)
