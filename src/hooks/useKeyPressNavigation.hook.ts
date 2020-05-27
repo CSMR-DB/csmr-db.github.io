1ab8f1123ab0c522087e52e36b260b1959af8a97
Casimir de Bruijn <casimir.db@gmail.com>
2020-05-27 15:30:53 +0200

    Added typedefs to useKeyPressNavigation hook

--- src/hooks/useKeyPressNavigation.hook.ts
+++ src/hooks/useKeyPressNavigation.hook.ts
@@ -1,31 +1,26 @@
-// import { navigate } from 'gatsby';
 import { IRouteObject } from './../data/routes';
 import { useState, useEffect } from 'react';
 
-// tslint:disable: typedef
 export function useKeyPressNavigation(routes: IRouteObject[]): string {
-  // State for keeping track of whether key is pressed
-  const [navigationPath, setNavigationPath] = useState('');
+  const [navigationPath, setNavigationPath]: [ string, React.Dispatch<React.SetStateAction<string>> ] = useState('');
 
-  const keyPressRouteBinds = new Map<string, string>()
-  routes.map(route => route.boundKeys.map( key => ( keyPressRouteBinds.set( `${key}`, route.path ))))
+  const keyPressRouteBinds: Map<string, string> = new Map()
+  routes.forEach((route: IRouteObject): void => { 
+    route.boundKeys.forEach((key: string | number): void => { 
+      keyPressRouteBinds.set( `${key}`, route.path )
+    }) 
+  })
 
   console.table(keyPressRouteBinds)
 
-  // Add event listeners
-  useEffect(() => {
-
-    // If pressed key is our target key then set to true
-    function downHandler({ key }: { key: string }) {
+  useEffect((): (() => void) => {
+    function downHandler({ key }: { key: string }): void {
       if (keyPressRouteBinds.has(key)) {
-        // tslint:disable-next-line: no-floating-promises
-        // navigate(`${keyPressRouteBinds.get(key)}`)
         setNavigationPath(`${keyPressRouteBinds.get(key)}`)
       }
     }
 
-    // If released key is our target key then set to false
-    const upHandler = ({ key }: { key: string }) => {
+    function upHandler({ key }: { key: string }): void {
       if (keyPressRouteBinds.has(key)) {
         setNavigationPath('');
       }
@@ -34,12 +29,11 @@ export function useKeyPressNavigation(routes: IRouteObject[]): string {
     window.addEventListener('keydown', downHandler);
     window.addEventListener('keyup', upHandler);
 
-    // Remove event listeners on cleanup
-    return () => {
+    return (): void => {
       window.removeEventListener('keydown', downHandler);
       window.removeEventListener('keyup', upHandler);
     };
-  }, [routes]); // Empty array ensures that effect is only run on mount and unmount
+  }, [routes]);
 
   return navigationPath;
 }
