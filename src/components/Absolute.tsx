871a6ee237fa50446bdd6358337e211344b05e92
Casimir de Bruijn <casimir.db@gmail.com>
2020-05-29 14:47:09 +0200

    Renamed 'Absolute' Styled-Component

--- src/components/Absolute.tsx
+++ src/components/Absolute.tsx
@@ -1,13 +1,30 @@
 import styled, { StyledComponent } from 'styled-components'
+import { ReactNode } from 'react'
+import { LeftProperty, TopProperty, WidthProperty, HeightProperty, BottomProperty, RightProperty } from 'csstype'
 
-export const AbsoluteCenter: StyledComponent<
+export interface IAbsoluteProps {
+  children: ReactNode
+  top?: TopProperty<1>
+  left?: LeftProperty<1>
+  bottom?: BottomProperty<1>
+  right?: RightProperty<1>
+  width?: WidthProperty<1>
+  height?: HeightProperty<1>
+}
+
+export const Absolute: StyledComponent<
   'div',
   any,
-  {},
+  IAbsoluteProps,
   never
 > = styled.div`
   position: absolute;
-  top: 0;
-  width: 100%;
-  height: 100%;
+  width: ${(props: IAbsoluteProps): WidthProperty<1> =>
+    props.width || '100%'};
+  height: ${(props: IAbsoluteProps): HeightProperty<1> =>
+    props.height || '100%'};
+  top: ${(props: IAbsoluteProps): TopProperty<1> => props.top || '0'};
+  left: ${(props: IAbsoluteProps): LeftProperty<1> => props.left || '0'};
+  bottom: ${(props: IAbsoluteProps): TopProperty<1> => props.bottom || '0'};
+  right: ${(props: IAbsoluteProps): TopProperty<1> => props.right || '0'};
 `
