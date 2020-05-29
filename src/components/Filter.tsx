8678f435333559301f98d4a59b986af2dfc2a3b7
Casimir de Bruijn <casimir.db@gmail.com>
2020-05-29 14:52:42 +0200

    Changed default 'Filter' settings for Wallpaper

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,8 @@
 import styled, { StyledComponent } from 'styled-components'
 
 export const Filter: StyledComponent<'div', any, {}, never> = styled.div`
-  filter: sepia(0.5) saturate(2) grayscale(0.75);
+  /* filter: sepia(0.5) saturate(2) grayscale(0.75); */
+  filter: sepia(0.25) saturate(2) brightness(0.75) blur(4px);
   transform: scale(1.2);
-  height: 100%;
+  /* height: 100%; */
 `
