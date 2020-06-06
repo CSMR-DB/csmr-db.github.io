6ec6f8cb69d1f30377353686ccf4779b2d8eb474
Casimir de Bruijn <casimir.db@gmail.com>
2020-06-06 17:39:40 +0200

    Modified default Filter settings (should use props next)

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,8 +1,5 @@
 import styled, { StyledComponent } from 'styled-components'
 
 export const Filter: StyledComponent<'div', any, {}, never> = styled.div`
-  /* filter: sepia(0.5) saturate(2) grayscale(0.75); */
-  filter: sepia(0.25) saturate(2) brightness(0.75) blur(4px);
-  transform: scale(1.2);
-  /* height: 100%; */
+  filter: sepia(0.25) saturate(2) brightness(0.75);
 `
