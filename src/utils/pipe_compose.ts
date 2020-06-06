b610f66039d379c377e220d4974c5fdb45383b34
Casimir de Bruijn <casimir.db@gmail.com>
2020-06-06 17:32:34 +0200

    Added premade pipe & compose functions as utils

---
+++ src/utils/pipe_compose.ts
@@ -0,0 +1,11 @@
+type PipeComposeFn = <T>(...fns: ((v: T) => T)[]) => (val: T) => T;
+
+export const pipe: PipeComposeFn = <T>(
+  ...fns: ((v: T) => T)[]
+): ((val: T) => T) => (val: T): T =>
+  fns.reduce((acc: T, fn: (v: T) => T): T => fn(acc), val);
+
+export const compose: PipeComposeFn = <T>(
+  ...fns: ((v: T) => T)[]
+): ((val: T) => T) => (val: T): T =>
+  fns.reduceRight((acc: T, fn: (v: T) => T): T => fn(acc), val);
