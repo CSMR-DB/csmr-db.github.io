da6dd4d5ea9f0499e16b44794a1c0291039a1510
Casimir de Bruijn <casimir.db@gmail.com>
2020-06-18 12:27:59 +0200

    changed: added rules

--- src/components/flex.tsx
+++ src/components/flex.tsx
@@ -1,12 +1,20 @@
 import React, { ReactNode } from 'react'
 import styled, { StyledComponent } from 'styled-components'
-import { BackgroundColorProperty, JustifyContentProperty, FlexDirectionProperty } from 'csstype'
+import {
+  BackgroundColorProperty,
+  JustifyContentProperty,
+  FlexDirectionProperty,
+  FlexWrapProperty,
+  AlignItemsProperty,
+} from 'csstype'
 
 export interface IFlexProps {
   children: ReactNode
   background?: BackgroundColorProperty
   justifyContent?: JustifyContentProperty
+  alignItems?: AlignItemsProperty
   direction?: FlexDirectionProperty
+  wrap?: FlexWrapProperty
 }
 
 const StyledFlex: StyledComponent<'div', any, IFlexProps, never> = styled.div`
@@ -15,10 +23,15 @@ const StyledFlex: StyledComponent<'div', any, IFlexProps, never> = styled.div`
     props.background || 'transparent'};
   flex-direction: ${(props: IFlexProps): FlexDirectionProperty =>
     props.direction || 'column'};
+  flex-wrap: ${(props: IFlexProps): FlexWrapProperty =>
+    props.wrap || 'initial'};
 
+  align-self: center;
   height: 100%;
+  width: 100%;
   display: flex;
-  align-items: center;
+  align-items: ${(props: IFlexProps): JustifyContentProperty =>
+    props.alignItems || 'center'};
   justify-content: ${(props: IFlexProps): JustifyContentProperty =>
     props.justifyContent || 'space-around'};
   transition: all 0.25s ease-in-out;
@@ -28,7 +41,14 @@ const StyledFlex: StyledComponent<'div', any, IFlexProps, never> = styled.div`
   }
 `
 
-export const Flex: React.ForwardRefExoticComponent<IFlexProps & React.RefAttributes<HTMLDivElement>> =
-  React.forwardRef((props: IFlexProps, ref: React.RefObject<HTMLDivElement> | ((instance: HTMLDivElement) => void) | null): JSX.Element => (
-    <StyledFlex {...props} ref={ref} />
-  ))
+export const Flex: React.ForwardRefExoticComponent<
+  IFlexProps & React.RefAttributes<HTMLDivElement>
+> = React.forwardRef(
+  (
+    props: IFlexProps,
+    ref:
+      | React.RefObject<HTMLDivElement>
+      | ((instance: HTMLDivElement) => void)
+      | null
+  ): JSX.Element => <StyledFlex {...props} ref={ref} />
+)
