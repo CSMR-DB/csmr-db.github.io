ec5875d6211711a0c856ae70758fa7a683484143
Casimir de Bruijn <casimir.db@gmail.com>
2020-06-18 12:25:46 +0200

    changed: added props to Filter

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,31 @@
-import styled, { StyledComponent } from 'styled-components'
+import styled, { css, FlattenSimpleInterpolation, StyledComponent } from 'styled-components';
+import { ColorProperty, FilterProperty } from 'csstype';
 
-export const Filter: StyledComponent<'div', any, {}, never> = styled.div`
-  filter: sepia(0.25) saturate(2) brightness(0.75);
+interface IFilterProps {
+  filter?: FilterProperty
+  filterHover?: FilterProperty
+  lens?: ColorProperty
+}
+
+export const Filter: StyledComponent<'div', any, IFilterProps, never> = styled.div`
+  position: relative;
+  filter: ${({ filter = 'sepia(0.25) saturate(2) grayscale(1) brightness(.5)' }: IFilterProps): FilterProperty => filter};
+  transition: all 200ms ease-in-out;
+  overflow: hidden;
+
+  &:hover {
+    filter: ${({ filterHover = 'none' }: IFilterProps): FilterProperty => filterHover};; 
+  }
+
+  ${({ lens }: IFilterProps): FlattenSimpleInterpolation | null => lens !== undefined ? css`
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: ${lens};
+    }
+  `: null}
 `
