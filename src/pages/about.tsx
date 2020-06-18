a51d1a9b6dc3cb2462305082ff911ca2d90b153a
Casimir de Bruijn <casimir.db@gmail.com>
2020-06-18 12:22:13 +0200

    changed: new implementation of the AboutLayout

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,88 @@
 import React from 'react'
 import { Layout } from '../components/Layout'
 import { AboutLayout } from '../layouts/AboutLayout'
+import { graphql, StaticQuery } from 'gatsby'
+import { IBioImagesMarkdownRemark } from '../components/the_movie/types.interface'
+
+// tslint:disable-next-line: no-void-expression
+const PAGE_QUERY: void = graphql`
+  fragment ImagesFragment on FileConnection {
+    edges {
+      node {
+        name
+        relativePath
+        childImageSharp {
+          fluid(maxWidth: 240, maxHeight: 240, cropFocus: CENTER){
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+
+  query {
+    wallpapers: allFile(filter: {
+      extension: { regex: "/(jpg)|(png)|(jpeg)/" }
+      relativeDirectory: { regex: "/bio/games/" }
+    }, limit: 14) {
+      edges {
+        node {
+          name
+          relativePath
+          childImageSharp {
+            fluid(maxWidth: 960, maxHeight: 540, cropFocus: CENTER){
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+
+    squares: allFile(filter: {
+      extension: { regex: "/(jpg)|(png)|(jpeg)/" }
+      relativeDirectory: { regex: "/photography/" }
+    }, limit: 9) {
+      edges {
+        node {
+          name
+          relativePath
+          childImageSharp {
+            fluid(maxWidth: 240, maxHeight: 240, cropFocus: CENTER){
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+
+    posters: allFile(filter: {
+      extension: { regex: "/(jpg)|(png)|(jpeg)/" }
+      relativeDirectory: { regex: "/bio/series/" }
+    }, limit: 28) {
+      edges {
+        node {
+          name
+          relativePath
+          childImageSharp {
+            fluid(maxWidth: 270, maxHeight: 480, cropFocus: CENTER){
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const AboutMe: () => JSX.Element = (): JSX.Element => (
   <Layout>
-    <AboutLayout />
+    {/* <AboutLayout /> */}
+    <StaticQuery
+      query={PAGE_QUERY}
+      render={(data: { wallpapers: IBioImagesMarkdownRemark, squares: IBioImagesMarkdownRemark, posters: IBioImagesMarkdownRemark }): JSX.Element => (
+        <AboutLayout {...data} />
+      )}
+    />
   </Layout>
 )
 
