09ec261f495fae1c78803e735b619f65c3cf3df6
Casimir de Bruijn <casimir.db@gmail.com>
2020-06-18 12:28:44 +0200

    added: sub-directory export from single file

---
+++ src/components/index.ts
@@ -0,0 +1,43 @@
+export { Absolute } from './Absolute'
+export { ArticleBody } from './ArticleBody'
+export { ArticleTags } from './ArticleTags'
+export { Blend } from './Blend'
+export {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardHeaderFeaturedImage,
+  CardHeaderIconWrapper,
+  CardHeaderText,
+} from './Card'
+export { CardHeaderImageOrVideo } from './CardHeaderImageOrVideo'
+export { CenteredBlock } from './CenteredBlock'
+export { ClippedBtn } from './ClippedBtn'
+export { ContentSeparator } from './ContentSeparator'
+export { CTAButton } from './CTAButton'
+export { DateFormatted } from './DateFormatted'
+export { DesktopNav } from './DesktopNav'
+export { DynamicIcon } from './DynamicIcon'
+export { DynamicImage } from './DynamicImage'
+export { ExperienceCard } from './ExperienceCard'
+export { Filter } from './Filter'
+export { Fixed } from './Fixed'
+export { Flex } from './Flex'
+export { Footer } from './Footer'
+export { Grid } from './Grid'
+export { Header } from './Header'
+export { Image } from './Image'
+export { ImageContainerCircle } from './ImageContainerCircle'
+export { Layout } from './Layout'
+export { Logo } from './Logo'
+export { MenuTile } from './MenuTile'
+export { ProjectCard } from './ProjectCard'
+export { ReadingTime } from './ReadingTime'
+export { Relative } from './Relative'
+export { SEO } from './SEO'
+export { SkillCard } from './SkillCard'
+export { SocialButton } from './SocialButton'
+export { StyledIFrame } from './StyledIFrame'
+export { StyledLink } from './StyledLink'
+export { WASDNav } from './WASDNav'
