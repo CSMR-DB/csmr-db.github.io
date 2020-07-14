100306bca464c80d67605ad6f3349f63a6957279
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-14 12:25:33 +0200

    changed: redesigned homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react'
-import { IndexLayout } from '../layouts/IndexLayout'
+import { graphql, useStaticQuery } from 'gatsby'
+import { IndexLayout, ISiteMetadata } from '../layouts/IndexLayout'
+import { IProjectsLayoutProps } from '../layouts/ProjectsLayout'
+import { ISkillsetLayoutProps } from '../layouts/SkillsetLayout'
 
-const IndexPage: () => JSX.Element = (): JSX.Element => (
-  <IndexLayout />
-)
+const IndexPage: () => JSX.Element = (): JSX.Element => {
+  const data: ISiteMetadata &
+    IProjectsLayoutProps &
+    // tslint:disable-next-line: no-void-expression
+    ISkillsetLayoutProps = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+        }
+      }
+
+      skillsetData: allMarkdownRemark(
+        filter: {
+          frontmatter: { path: { regex: "/tag/" }, favorite: { eq: true } }
+        }
+        sort: { fields: frontmatter___time, order: DESC }
+        limit: 3
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+              excerpt
+              level
+              skillColor
+              time
+              favorite
+            }
+            excerpt
+          }
+        }
+      }
+
+      programmingData: allMarkdownRemark(
+        filter: {
+          frontmatter: {
+            path: { regex: "/projects/" }
+            category: { eq: "Programming" }
+            favorite: { eq: true }
+          }
+        }
+        sort: { fields: frontmatter___date, order: DESC }
+        limit: 3
+      ) {
+        ...SharedOPQuery
+      }
+    }
+  `)
+
+  return <IndexLayout data={data} />
+}
 
 // tslint:disable-next-line: no-default-export
 export default IndexPage
