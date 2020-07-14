5e1b5c5e2dc4e28993b984d0e142c423f1d8f0a5
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-14 12:37:33 +0200

    changed: added padding on "mobile" view

--- src/components/CenteredBlock.tsx
+++ src/components/CenteredBlock.tsx
@@ -1,5 +1,6 @@
 import styled, { StyledComponent } from 'styled-components'
 import { WidthProperty } from 'csstype'
+import { theme } from '../data/theme'
 
 interface ICenteredBlockProps {
   maxWidth?: WidthProperty<1>
@@ -15,4 +16,8 @@ export const CenteredBlock: StyledComponent<
     maxWidth = '48rem',
   }: ICenteredBlockProps): WidthProperty<1> => maxWidth};
   margin: 0 auto;
+
+  @media ${theme.breakpoints.max.smartphone} {
+    padding: 0 1rem;
+  }
 `
