3db6fd4bd8e57d7c47a7fa958fecc46e0b58ad87
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-14 13:05:14 +0200

    changed: made `Heart` a ForwardRef Component

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled, { StyledComponent } from 'styled-components'
 import { FillProperty, HeightProperty, WidthProperty } from 'csstype'
+import {
+  AnyExoticRefComponent,
+  AnyExoticRefTargets,
+} from './the_movie/types.interface'
 
 interface IHeartProps {
   height: HeightProperty<1>
@@ -17,12 +21,12 @@ export const StyledHeart: StyledComponent<
   fill: ${({ fill }: IHeartProps): FillProperty => fill};
 `
 
-export function Heart(props: IHeartProps): JSX.Element {
-  return (
-    <StyledHeart {...props} viewBox="0 0 315 342">
+export const Heart: AnyExoticRefComponent<IHeartProps> = React.forwardRef(
+  (props: IHeartProps, ref: AnyExoticRefTargets): JSX.Element => (
+    <StyledHeart {...props} viewBox="0 0 315 342" ref={ref}>
       <g id="heart">
         <path d="M0 200 v-200 h200 a100,100 90 0,1 0,200 a100,100 90 0,1 -200,0 z" />
       </g>
     </StyledHeart>
   )
-}
+)
