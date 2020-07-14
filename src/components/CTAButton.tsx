eb39604c081a69d5a87163ca909ff84c05f0e877
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-14 12:43:41 +0200

    changed: renamed props because React complained

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled, { StyledComponent } from 'styled-components'
 import { MarginProperty } from 'csstype'
+import { theme } from '../data/theme'
 
 interface IStyledCTAProps {
-  center: boolean
+  iscentered: string
 }
 
-export const StyledCTAButton: StyledComponent<typeof Link, any, IStyledCTAProps, never> = styled(Link)`
+export const StyledCTAButton: StyledComponent<
+  typeof Link,
+  any,
+  IStyledCTAProps,
+  never
+> = styled(Link)`
   @keyframes RGB {
-    0%, 100% {
+    0%,
+    100% {
       background: red;
     }
     33% {
@@ -26,27 +33,39 @@ export const StyledCTAButton: StyledComponent<typeof Link, any, IStyledCTAProps,
   /* animation: 4000ms RGB infinite linear; */
   border: 4px dotted black;
   font-size: 1.25rem;
-  margin: ${({ center }: IStyledCTAProps): MarginProperty<1> => center ? 'auto' : ''};
-  margin-top: 8rem;
+  margin: ${({ iscentered }: IStyledCTAProps): MarginProperty<1> =>
+    iscentered === 'true' ? 'auto' : ''};
+  /* margin-top: 8rem; */
   cursor: pointer;
   color: white !important;
   text-transform: uppercase;
   text-align: center;
+  text-decoration: none;
   padding: 1.5rem;
   max-width: 24rem;
   border: none;
   border-radius: 999rem;
 
   &:hover {
-    filter: grayscale(.5);
+    filter: grayscale(0.5);
+  }
+
+  @media ${theme.breakpoints.max.smartphone} {
+    font-size: 1rem;
   }
 `
 
-export function CTAButton({ to, center = true, children }: { to: string, center?: boolean, children: React.ReactNode }): JSX.Element {
+export function CTAButton({
+  to,
+  iscentered = true,
+  children,
+}: {
+  to: string
+  iscentered?: boolean
+  children: React.ReactNode
+}): JSX.Element {
   return (
-    <StyledCTAButton
-      to={to}
-      center={center}>
+    <StyledCTAButton to={to} iscentered={iscentered.toString()}>
       {children}
     </StyledCTAButton>
   )
