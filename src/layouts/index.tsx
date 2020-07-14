4344b9c10a77e740a19b28309eff67c6ee61d0fa
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-14 12:14:58 +0200

    added: Page Transitions using `gatsby-plugin-transitions`

---
+++ src/layouts/index.tsx
@@ -0,0 +1,72 @@
+import React, { ReactNode } from 'react'
+// tslint:disable-next-line: no-import-side-effect
+import '../css/reset.css'
+import { TransitionProvider, TransitionViews } from 'gatsby-plugin-transitions'
+import { Header, Footer } from '../components'
+import { IONavigator } from '../hooks/IONavigator'
+
+interface ILayoutProps {
+  children: ReactNode
+  location: {
+    hash: string
+    host: string
+    hostname: string
+    href: string
+    key: string
+    origin: string
+    pathname: string
+    port: string
+    protocol: string
+    search: string
+    state: {
+      key: string
+    }
+  }
+}
+
+function TransitioningLayout({
+  children,
+  location,
+}: ILayoutProps): JSX.Element {
+  const isLandingPage: boolean = location.pathname === '/'
+
+  return (
+    <IONavigator>
+      <TransitionProvider
+        location={location}
+        // mode="immediate"
+        enter={{
+          opacity: 0,
+          transform: 'scale(1)',
+          config: {
+            mass: 1,
+            tension: 210,
+            friction: 20,
+            clamp: true,
+          },
+        }}
+        usual={{
+          opacity: 1,
+          transform: 'scale(1)',
+        }}
+        leave={{
+          opacity: 0,
+          transform: 'scale(1)',
+          config: {
+            duration: 600,
+          },
+        }}
+      >
+        <Header
+          height={isLandingPage ? '100vh' : '24rem'}
+          isLandingPage={isLandingPage}
+        />
+        <TransitionViews>{children}</TransitionViews>
+        <Footer />
+      </TransitionProvider>
+    </IONavigator>
+  )
+}
+
+// tslint:disable-next-line: no-default-export
+export default TransitioningLayout
