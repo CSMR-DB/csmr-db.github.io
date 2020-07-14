83858797c517311b3871e8619097d969c0db1148
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-14 12:26:19 +0200

    changed: shortened copy & prettierPocalypse

--- src/layouts/SkillsetLayout.tsx
+++ src/layouts/SkillsetLayout.tsx
@@ -4,7 +4,6 @@ import { SkillCard } from '../components/SkillCard'
 import { ExperienceCard } from '../components/ExperienceCard'
 import { CenteredBlock } from '../components/CenteredBlock'
 import { Grid } from '../components/Grid'
-import { StyledLink } from '../components/StyledLink'
 import {
   IExperienceAllMarkdownRemark,
   IExperienceEdge,
@@ -18,7 +17,10 @@ export interface ISkillsetLayoutProps {
   experienceData: IExperienceAllMarkdownRemark
 }
 
-export function SkillsetLayout({ skillsetData, experienceData }: ISkillsetLayoutProps): JSX.Element {
+export function SkillsetLayout({
+  skillsetData,
+  experienceData,
+}: ISkillsetLayoutProps): JSX.Element {
   return (
     <>
       <SEO
@@ -27,24 +29,20 @@ export function SkillsetLayout({ skillsetData, experienceData }: ISkillsetLayout
       />
       <CenteredBlock>
         <ContentSeparator>
-          <h1>So, you want to know more?</h1>
+          <h1>This is what I have worked with</h1>
           <p>
-            From my experience so far, programming is about 20% actually
-            writing code and 80% "looking stuff up", gathering requirements,
-            defining test scenarios. Obviously, this can vary on a daily
-            basis. But when it comes to actually creating, how do I think I'd
-            score? The time spent is a rough estimate and should be seen as a
-            total of the 20 / 80 split.
-            </p>
+            A rough estimation of the total time I've spent on everything is
+            included.
+          </p>
         </ContentSeparator>
       </CenteredBlock>
       <ContentSeparator>
-        <Grid
-          columns="repeat(auto-fill, minmax(25rem, 1fr))"
-          maxWidth="160rem"
-        >
+        <Grid columns="repeat(auto-fill, minmax(25rem, 1fr))" maxWidth="160rem">
           {skillsetData.edges.map(
-            ({ node: { frontmatter } }: ISkillsetEdge, i: number): JSX.Element => (
+            (
+              { node: { frontmatter } }: ISkillsetEdge,
+              i: number
+            ): JSX.Element => (
               <SkillCard
                 level={frontmatter.level || 0}
                 title={frontmatter.title}
@@ -58,25 +56,6 @@ export function SkillsetLayout({ skillsetData, experienceData }: ISkillsetLayout
           )}
         </Grid>
       </ContentSeparator>
-      <CenteredBlock>
-        <p>
-          I would like to point out that these scores are more "relative" to
-          each other than "absolute". Whatever the subject, there's still a
-          lot to learn. I also know that whatever problem I may face dealing
-          with code, eventually a solution will come. I also know that
-          becoming an expert in any field takes at least 10.000 hours of
-          practise. More often than not, I'll be browsing Medium to look for
-          new blogposts regarding JS. Always trying to improve. Most
-          importantly investing more time in JavaScript / TypeScript than any
-          specific framework, so that it'll be easier to pick up whatever
-          comes next.
-          </p>
-        <p>
-          But, as I also mention in the{' '}
-          <StyledLink to="/about">about me section</StyledLink>, I have some
-            experience with other programs outside of programming.
-          </p>
-      </CenteredBlock>
       <CenteredBlock>
         <ContentSeparator>
           <h1>Education</h1>
@@ -95,7 +74,10 @@ export function SkillsetLayout({ skillsetData, experienceData }: ISkillsetLayout
           <h1>Professional experience</h1>
           <Grid>
             {experienceData.edges.map(
-              ({ node: { frontmatter } }: IExperienceEdge, i: number): JSX.Element => (
+              (
+                { node: { frontmatter } }: IExperienceEdge,
+                i: number
+              ): JSX.Element => (
                 <ExperienceCard
                   name={frontmatter.title}
                   type={frontmatter.type}
