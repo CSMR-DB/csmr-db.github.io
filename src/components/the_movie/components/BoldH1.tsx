9969a16ef909d18d1e6b575486d45994fea82ebe
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-23 14:50:34 +0200

    changed: Removed "the movie"

--- src/components/the_movie/components/BoldH1.tsx
+++
@@ -1,8 +0,0 @@
-import styled, { StyledComponent } from 'styled-components'
-
-export const BoldH1: StyledComponent<'h1', any, {}, never> = styled.h1`
-  border-radius: 0.25rem;
-  background: white;
-  padding: 2rem;
-  margin: 0;
-`
