f6c812d737340f14ea4a7a392b2023122ed3c0e3
Casimir de Bruijn <casimir.db@gmail.com>
2020-07-24 13:24:00 +0200

    changed: removed `Tween` from `SkillCardBody`

--- src/components/compositions/card/skillcard/SkillCard.tsx
+++ src/components/compositions/card/skillcard/SkillCard.tsx
@@ -73,7 +73,7 @@ export function SkillCard({
         </SkillCardHeader>
       </CardHeader>
       <CardBody>
-        <SkillCardBody description={description} index={index} />
+        <SkillCardBody description={description} />
       </CardBody>
       <CardFooter>
         <p>
--- src/components/compositions/card/skillcard/SkillCardBody.tsx
+++ src/components/compositions/card/skillcard/SkillCardBody.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Tween } from 'react-gsap'
 
 import { StringOrUrlArray } from '../../../../types/StringOrUrlObject'
 
@@ -7,20 +6,9 @@ import { ArticleBody } from '../../../ArticleBody'
 
 interface ISkillCardBodyProps {
   description: StringOrUrlArray
-  index: number
 }
 export function SkillCardBody({
   description,
-  index,
 }: ISkillCardBodyProps): JSX.Element {
-  return (
-    <Tween
-      from={{ opacity: 0 }}
-      duration={1}
-      delay={1 + index * 0.125}
-      ease="power1"
-    >
-      <ArticleBody body={description} />
-    </Tween>
-  )
+  return <ArticleBody body={description} />
 }
